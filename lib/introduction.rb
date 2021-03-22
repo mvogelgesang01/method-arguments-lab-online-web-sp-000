a7beef00785e21b4adc018133db247e4c61762aa
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-22 21:51:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,13 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
+def name(name)
+  puts ("Dan")
+end
+
+def language(language)
+  puts ("Ember.js")
+end
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
